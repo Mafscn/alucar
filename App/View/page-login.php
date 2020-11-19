64e24b348c4a42b4615ab6a085e203fef6f57dc6
MARCOS RAFAEL GIULIANETTI DE OLIVEIRA <marcos.oliveira121@fatec.sp.gov.br>
2020-11-19 09:37:59 -0300

    Teste no git

--- App/View/page-login.php
+++ App/View/page-login.php
@@ -12,22 +12,11 @@
     <div class="row">
         <div class="col-sm col-6">
             <? require("widgets/alucar-logo.php"); ?>
-<<<<<<< HEAD
-            <div style="height: 2rem;"></div>
-            <form autocomplete="off" method="POST" action="../Controller/pdo_login.php">
-                <input type="email" class="form-campo" placeholder="Email" name="email">
-                <div style="height: 1rem;"></div>
-                <input type="password" class="form-campo" placeholder="Password" name="password">
-                <div style="height: 2rem;"></div>
-                <input type="submit" class="btn-bg" role="button" name="entrar" value="Entrar">
-                <div style="height: 2rem;"></div>
-=======
             <form autocomplete="off">
                 <input type="email" class="form-campo" placeholder="Email">
                 <input type="password" class="form-campo" placeholder="Password">
                 <a href="page-aluguel.php" class="btn-bg" role="button">Entrar</a>
                 <br>
->>>>>>> 2ff2516b9925305c2375647b67cd088897ea5539
                 <a href="page-cadastro.php" class="btn-sm" role="button">Criar uma Conta</a>
                 <br>
                 <a href="page-recuperar.php" class="btn-sm" role="button">Recuperar Conta</a>
