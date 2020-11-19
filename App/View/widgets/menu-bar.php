f411bffcb3f1ac2069e2d5fcf37461e060ede1ed
Mafscn <mafscn@gmail.com>
2020-11-19 12:30:40 -0300

    teste

--- App/View/widgets/menu-bar.php
+++ App/View/widgets/menu-bar.php
@@ -25,30 +25,3 @@
         </div>
     </div>
 </div>
-<div class="d-none col-3">
-    <div class="d-none fixed">
-        <div style="margin-left: 10%;">
-            <? include("widgets/alucar-logo.php") ?>
-        </div>
-        <div class="btn-container">
-            <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">monetization_on</i>
-            <a href="page-aluguel.php">Aluguel</a>
-        </div>
-        <div class="btn-container">
-            <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">person</i>
-            <a href="page-clientes.php">Clientes</a>
-        </div>
-        <div class="btn-container">
-            <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">directions_car</i>
-            <a href="page-veiculos.php">Veículos</a>
-        </div>
-        <div class="btn-container">
-            <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">settings</i>
-            <a href="page-conta.php">Conta</a>
-        </div>
-    </div>
-</div>
