56c029bd31dc92bd629b3b264e6622c0a5b9eaba
Mafscn <mafscn@gmail.com>
2020-11-20 09:38:54 -0300

    Criação: menu-bar-mobile

--- App/View/widgets/menu-bar.php
+++ App/View/widgets/menu-bar.php
@@ -1,27 +1,62 @@
 <div class="d-none col-3">
-    <div class="d-none fixed">
+    <div class="fixed">
         <div style="margin-left: 10%;">
             <? include("widgets/alucar-logo.php") ?>
         </div>
         <div class="btn-container">
             <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">monetization_on</i>
-            <a href="page-aluguel.php">Aluguel</a>
+            <a href="page-aluguel.php" class="menu-btn">
+                <i class="material-icons" style="font-size: 3rem;">monetization_on</i>
+                <div style="margin-top: 6px;">Aluguel</div>
+            </a>
         </div>
         <div class="btn-container">
             <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">person</i>
-            <a href="page-clientes.php">Clientes</a>
+            <a href="page-clientes.php" class="menu-btn">
+                <i class="material-icons" style="font-size: 3rem;">person</i>
+                <div style="margin-top: 6px;">Clientes</div>
+            </a>
         </div>
         <div class="btn-container">
             <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">directions_car</i>
-            <a href="page-veiculos.php">Veículos</a>
+            <a href="page-veiculos.php" class="menu-btn">
+                <i class="material-icons" style="font-size: 3rem;">directions_car</i>
+                <div style="margin-top: 6px;">Veículos</div>
+            </a>
         </div>
         <div class="btn-container">
             <div class="btn-menu"></div>
-            <i class="material-icons menu" style="font-size: 3rem;">settings</i>
-            <a href="page-conta.php">Conta</a>
+            <a href="page-conta.php" class="menu-btn">
+                <i class="material-icons" style="font-size: 3rem;">settings</i>
+                <div style="margin: 6px 28px 0 0;">Conta</div>
+            </a>
         </div>
+    </div>
+</div>
+
+<div class="menu-bar-mobile">
+    <div class="btn-container">
+        <div class="btn-menu-mobile"></div>
+        <a href="page-aluguel.php" style="padding-top: 7px;">
+            <i class="material-icons" style="font-size: 3rem;">monetization_on</i>
+        </a>
+    </div>
+    <div class="btn-container">
+        <div class="btn-menu-mobile"></div>
+        <a href="page-clientes.php" style="padding-top: 7px;">
+            <i class="material-icons" style="font-size: 3rem;">person</i>
+        </a>
+    </div>
+    <div class="btn-container">
+        <div class="btn-menu-mobile"></div>
+        <a href="page-veiculos.php">
+            <i class="material-icons" style="font-size: 3rem;">directions_car</i>
+        </a>
+    </div>
+    <div class="btn-container">
+        <div class="btn-menu-mobile"></div>
+        <a href="page-conta.php" style="padding-top: 7px;">
+            <i class="material-icons" style="font-size: 3rem;">settings</i>
+        </a>
     </div>
 </div>
