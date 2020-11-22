d7b54bb3c15e70c8606ff74ae38d9bea46a4503d
Psilva009 <marips0202@gmail.com>
2020-11-22 17:05:48 -0300

    Revert "Merge branch 'master' of https://github.com/Mafscn/alucar"
    This reverts commit eabb8e865bae492c134aeeedfd5ed0057123a382, reversing
    changes made to 1a6b44bc3d768a0f6b492bd4ba45ac27d4fdfefa.

--- App/View/page-login.php
+++ App/View/page-login.php
@@ -4,64 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="./App/View/bootstrap/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="./App/View/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/style.css">
-
-=======
     <link rel="stylesheet" href="style.css">
->>>>>>> 1a6b44bc3d768a0f6b492bd4ba45ac27d4fdfefa
     <title>Login</title>
 </head>
 
 <body>
     <div class="row">
-<<<<<<< HEAD
-        <div class="col-sm-12 col-lg-6">
-            <!-- Logo -->
-            <div class="container-logo">
-                <div class="row">
-                    <div class="col-6">
-                        <div class="container-img-logo">
-                            <img class="img-fluid" src="./App/View/images/placeholder-logo.png"
-                                style="border-radius: 50%; width: 110px;" alt="logo">
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="container-alucar">
-                            <h1>Alucar</h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Campos -->
-            <form class="signin-form">
-                <div class="form-group">
-                    <input type="email" class="form-control form-campo" placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control form-campo" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <a href="page-aluguel.php" class="btn btn-grande" role="button">Entrar</a>
-                </div>
-                <div class="form-group">
-                    <a href="page-cadastro.php" class="btn btn-pequeno" role="button">Criar uma Conta</a>
-                </div>
-                <div class="form-group">
-                    <a href="page-recuperar.php" class="btn btn-pequeno" role="button">Recuperar Conta</a>
-                </div>
-            </form>
-        </div>
-
-        <!-- imagem a esquerda -->
-        <div class="col-lg-6">
-            <div class="container-car-imgs">
-                <img src="./App/View/images/placeholder-car1.png" alt="carro" style="width: 850px;">
-=======
         <div class="col-sm col-6">
             <? require("widgets/alucar-logo.php"); ?>
             <form autocomplete="off" action="../Controller/pdo_login.php" method="POST">
@@ -77,7 +25,6 @@
         <div class="d-none col-6">
             <div class="container-car-img">
                 <img src="images/placeholder-car1.png" alt="carro">
->>>>>>> 1a6b44bc3d768a0f6b492bd4ba45ac27d4fdfefa
             </div>
         </div>
     </div>
--- routes/index.php
+++ routes/index.php
@@ -1,26 +1,15 @@
 <?php
-    use App\Controllers\LoginController;
     use function src\slimConfiguration;
 
-    use Psr\Http\Message\{ServerRequestInterface as Request, ResponseInterface as Response};
-    
+
     $app = new \Slim\App(slimConfiguration());
-    $container = $app->getContainer();
-    $container['view'] = new \Slim\Views\PhpRenderer('App/View/');
 
     // ================================================
 
-            $app->get('/', function (Request $request, Response $response) {
-                $response = $this->view->render($response, 'page-login.php');
-                return $response;
-            });
-
-            $app->get('/teste', function (Request $request, Response $response) {
-                $response = $this->view->render($response, 'teste.php');
-                return $response;
-            });
-
+    $app->get('/', function($request, $response, $args){
+        return $response->getBody()->write('Bem vindo ao Slim');
+    });
+    
     // ================================================
-
 
     $app->run();
