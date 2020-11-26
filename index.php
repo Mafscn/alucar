51514f1c12e7d491050fde08e9a59f6de208b5ba
Psilva009 <marips0202@gmail.com>
2020-11-26 10:00:33 -0300

    refactor: :art: Project restructuring
    Taken slim related folders

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php 
 
-require_once "./vendor/autoload.php";
-require_once "./env.php";
-require_once "./src/slimConfiguration.php";
-require_once "./routes/index.php";
-
 session_start();
 
 if($_SESSION['login'] == false){
-    header('Location: /alucar/App/View/page-login.php');
+    header('Location: /App/View/page-login.php');
     die();
 }else{
-    header('Location: /alucar/App/View/page-conta.php');
+    header('Location: /App/View/page-login.php');
     die();
 }
